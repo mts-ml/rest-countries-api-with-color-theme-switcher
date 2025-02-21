761d06be6a72c3c57f5e06c09845e2881b4199c3
Mateus <mtsmlima@gmail.com>
2025-02-21 13:48:16 -0300

    Added error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import Country from './pages/Country/Country';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 
 import './styles/globalStyle.scss';
 
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route element={<Layout />}>
+  <Route element={<Layout />} errorElement={<ErrorPage />}>
     <Route path="/" element={<Home />} />
     <Route path="country/:id" element={<Country />} />
   </Route>
